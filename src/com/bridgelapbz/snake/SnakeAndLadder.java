afbf9357a25d07eaf27155105eeb68d26b23b002
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 21:03:21 +0530

    Added: Start Game With Single Player

--- src/com/bridgelapbz/snake/SnakeAndLadder.java
+++ src/com/bridgelapbz/snake/SnakeAndLadder.java
@@ -1,5 +1,14 @@
 package com.bridgelapbz.snake;
 
 public class SnakeAndLadder {
-
+      
+        public static void main(String args[]) {
+        	
+                 System.out.println("Welcome To Snake Lader Game");
+                 int position=0;
+                 System.out.println("Start with Zero Position"+" " +position);
+                 int die=(int)(Math.floor(Math.random())*10)%6+1;
+                 System.out.println(die);
+               }
 }
+
