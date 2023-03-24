f27bd7ba2f1d3e3201a527612a32887c88fe80ac
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 21:09:44 +0530

    Added: The Number Roll Dice

--- src/com/bridgelapbz/snake/SnakeAndLadder.java
+++ src/com/bridgelapbz/snake/SnakeAndLadder.java
@@ -1,14 +1,11 @@
 package com.bridgelapbz.snake;
 
 public class SnakeAndLadder {
-      
-        public static void main(String args[]) {
-        	
-                 System.out.println("Welcome To Snake Lader Game");
-                 int position=0;
-                 System.out.println("Start with Zero Position"+" " +position);
-                 int die=(int)(Math.floor(Math.random())*10)%6+1;
-                 System.out.println(die);
-               }
-}
 
+	public static void main(String args[]) {
+
+		System.out.println("The Player rolls the die to get a number");
+		int value = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println(value);
+	}
+}
