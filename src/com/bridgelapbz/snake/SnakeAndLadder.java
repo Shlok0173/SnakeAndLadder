bce3886c19f4239384b0c4e5cb6d5a8fe4ce7bd6
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-03-24 22:00:18 +0530

    Added:played game

--- src/com/bridgelapbz/snake/SnakeAndLadder.java
+++ src/com/bridgelapbz/snake/SnakeAndLadder.java
@@ -3,40 +3,45 @@ package com.bridgelapbz.snake;
 import java.util.Scanner;
 
 public class SnakeAndLadder {
+	 static int count = 0;
 
-	public static void main(String args[]) {
-		
-			// TODO Auto-generated method stub
-			   int playerPosition = 0;
-		        Scanner s = new Scanner(System.in);
-		        System.out.println("Enter Player Name: ");
-		        String playerName = s.next();
-		        int winningPosition = 100;
+	    public static void main(String[] args) {
+	        int playerPosition = 0;
+	        Scanner s = new Scanner(System.in);
+	        System.out.println("Enter Player Name: ");
+	        String playerName = s.next();
+	        int winningPosition = 100;
+	        int remainingPosition = winningPosition - playerPosition;
 
-		        while (winningPosition > playerPosition) {
+	        while (winningPosition > playerPosition) {
 
-		            int diceRoll = (int) Math.floor(Math.random() * 10) % 6 + 1;
-		            System.out.println("The Number of dice is:" + diceRoll);
+	            int diceRoll = (int) Math.floor(Math.random() * 10) % 6 + 1;
+	            System.out.println("The no on dice is:" + diceRoll);
 
-		            int option = (int) Math.floor(Math.random() * 10) % 3;
 
-		            if (option == 0) {
-		                //System.out.println("No Play");
-		                playerPosition += 0;
-		            } else if (option == 1) {
-		                //System.out.println("Ladder");
-		                playerPosition += diceRoll;
-		            } else {
-		                //System.out.println("Snake");
-		                playerPosition -= diceRoll;
+	            int option = (int) Math.floor(Math.random() * 10) % 3;
+	            if (remainingPosition >= diceRoll) {
 
-		                if (playerPosition < 0) {
-		                    playerPosition = 0;
-		                }
+	                if (option == 0) {
+	                    //System.out.println("No Play");
+	                    playerPosition += 0;
+	                } else if (option == 1) {
+	                    //System.out.println("Ladder");
+	                    playerPosition += diceRoll;
+	                } else {
+	                    //System.out.println("Snake");
+	                    playerPosition -= diceRoll;
 
-		            }
-		            System.out.println("Player Position is:" + playerPosition);
+	                    if (playerPosition < 0) {
+	                        playerPosition = 0;
+	                    }
+	                }
+	                count++;
+	                System.out.println("Player Position is:" + playerPosition);
 
-		        }
-	}
+	            }
+	        }
+	        System.out.println("the Dice roll for :" + count);
+	    }
+
 }
